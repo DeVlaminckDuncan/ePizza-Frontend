250ee706329189bab083748bacb0ec1e98c7c612
Duncan <duncan.de.vlaminck@student.howest.be>
2020-10-31 19:12:59 +0100

    Basic delivery design

---
+++ src/presentations/pizza/customer/components/InputField.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="relative mb-6">
+		<input class="input block border border-dark border-opacity-25 rounded shadow-md px-3 py-2 focus:outline-none w-full" type="text" required />
+		<label class="label absolute text-dark text-opacity-75 bg-white px-3 py-1 pointer-events-none">{{ label }}</label>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	props: {
+		label: String,
+	},
+});
+</script>
+
+<style lang="scss">
+.label {
+	left: 4px;
+	top: 6px;
+	transition: 0.2s ease all;
+	-moz-transition: 0.2s ease all;
+	-webkit-transition: 0.2s ease all;
+}
+
+.input:focus,
+.input:valid {
+	// @apply border-alpha-green;
+
+	& ~ .label {
+		padding: 2px 12px;
+		top: -16px;
+		font-size: 14px;
+		// @apply text-alpha-green;
+	}
+}
+</style>
--- src/presentations/pizza/customer/views/Delivery.vue
+++ src/presentations/pizza/customer/views/Delivery.vue
@@ -1,9 +1,36 @@
 <template>
-	<h1>Delivery</h1>
+	<main class="px-6 py-8 flex justify-center">
+		<div class="main">
+			<h1 class="text-dark font-semibold text-2xl">Enter your address</h1>
+
+			<form class="w-full mt-8">
+				<div class="flex">
+					<InputField :label="'Street'" class="w-3/4 mr-3" />
+					<InputField :label="'No'" class="w-1/4" />
+				</div>
+
+				<InputField :label="'City'" />
+
+				<button class="text-white font-semibold text-lg bg-alpha-red px-12 py-2 mt-3 rounded-lg w-full shadow-md">Continue</button>
+			</form>
+		</div>
+	</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import InputField from '../components/InputField.vue';
+
+export default defineComponent({
+	components: {
+		InputField: InputField,
+	},
+});
 </script>
+
+<style lang="scss">
+.main {
+	width: 312px;
+}
+</style>
