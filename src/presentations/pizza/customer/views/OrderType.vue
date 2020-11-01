0889e5ca7757cb2b9cb0d76e119f8ccf58dd1ff9
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-01 17:20:35 +0100

    Basic menu design + changed some small stuff

--- src/presentations/pizza/customer/views/OrderType.vue
+++ src/presentations/pizza/customer/views/OrderType.vue
@@ -1,11 +1,13 @@
 <template>
-	<main class="px-6 py-8">
-		<h1 class="text-dark font-semibold text-2xl">Choose the order type</h1>
-		<div class="flex justify-center mt-8">
-			<router-link to="/delivery" class="text-white font-semibold text-xl bg-alpha-yellow px-12 py-3 rounded-lg w-64 shadow-md">Delivery</router-link>
-		</div>
-		<div class="flex justify-center mt-6">
-			<router-link to="/takeaway" class="text-white font-semibold text-xl bg-alpha-orange px-12 py-3 rounded-lg w-64 shadow-md">Takeaway</router-link>
+	<main class="px-6 py-8 flex justify-center">
+		<div class="main">
+			<h1 class="text-dark font-semibold text-2xl">Choose the order type</h1>
+			<div class="flex justify-center mt-8">
+				<router-link to="/delivery" class="button-lg text-white font-semibold text-xl bg-alpha-yellow px-12 py-3 rounded-lg shadow-md">Delivery</router-link>
+			</div>
+			<div class="flex justify-center mt-6">
+				<router-link to="/takeaway" class="button-lg text-white font-semibold text-xl bg-alpha-orange px-12 py-3 rounded-lg shadow-md">Takeaway</router-link>
+			</div>
 		</div>
 	</main>
 </template>
@@ -15,3 +17,13 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({});
 </script>
+
+<style lang="scss">
+.main {
+	width: 348px;
+}
+
+.button-lg {
+	width: 312px;
+}
+</style>
