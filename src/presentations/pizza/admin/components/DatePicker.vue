f12782f2963cda8e38d31ae43d9775b968dbd7cf
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-04 20:22:09 +0100

    Basic orders design + basic DatePicker component

---
+++ src/presentations/pizza/admin/components/DatePicker.vue
@@ -0,0 +1,23 @@
+<template>
+	<button @click="buttonClicked" class="h-8 w-48 flex justify-center items-center border border-dark border-opacity-10 rounded shadow-md focus:outline-none">
+		<span class="text-lg font-semibold opacity-80 mr-3">Change period</span>
+		<svg class="opacity-80" width="20" height="20" viewBox="0 0 24 24">
+			<path d="M0 0h24v24H0z" fill="none" />
+			<path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z" />
+		</svg>
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {
+		const buttonClicked = () => {};
+
+		return {
+			buttonClicked: buttonClicked,
+		};
+	},
+});
+</script>
--- src/presentations/pizza/admin/views/Orders.vue
+++ src/presentations/pizza/admin/views/Orders.vue
@@ -1,9 +1,74 @@
 <template>
-	<h1>Orders</h1>
+	<main class="px-6 py-8 flex justify-center">
+		<div class="main">
+			<h1 class="font-semibold text-2xl">Today's orders</h1>
+
+			<DatePicker class="mt-4" />
+
+			<div class="flex flex-wrap items-center border border-dark border-opacity-5 rounded-lg shadow-md p-3 mt-8">
+				<div class="w-full flex justify-between">
+					<div class="font-semibold">
+						Order #123456
+					</div>
+					<div>
+						01/12/2020
+					</div>
+				</div>
+
+				<div class="w-full text-left">
+					Customer #123456
+				</div>
+
+				<div class="w-full flex justify-between">
+					<div class="font-semibold">
+						Delivery
+					</div>
+					<div class="text-right">
+						Barnes Avenue 3081,
+						<br />
+						Cincinnati
+					</div>
+				</div>
+
+				<div class="w-full flex justify-between text-lg mt-1">
+					<div class="flex flex-col items-start">
+						<span>1 x Pizza margherita</span>
+						<span class="text-base opacity-80">Medium</span>
+					</div>
+					<div>
+						€ 8.00
+					</div>
+				</div>
+
+				<div class="w-full flex justify-between items-center mt-6">
+					<div class="text-xl">
+						Total
+					</div>
+					<div class="price font-semibold">
+						€ 8.00
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import DatePicker from '../components/DatePicker.vue';
+
+export default defineComponent({
+	components: {
+		DatePicker: DatePicker,
+	},
+
+	setup() {
+		const changePeriod = () => {};
+
+		return {
+			changePeriod: changePeriod,
+		};
+	},
+});
 </script>
