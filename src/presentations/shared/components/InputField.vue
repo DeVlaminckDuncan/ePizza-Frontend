588dfd92d3a4aef8d0ecbb48c1ffde8bc053f7be
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-09 23:15:46 +0100

    Added inputs + change image button

--- src/presentations/shared/components/InputField.vue
+++ src/presentations/shared/components/InputField.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="relative mb-6">
-		<input class="input block border border-dark border-opacity-25 rounded shadow-md px-3 py-2 focus:outline-none w-full" :type="type ? type : 'text'" required />
+		<input class="input block border border-dark border-opacity-25 rounded shadow-md px-3 py-2 focus:outline-none w-full" :type="type ? type : 'text'" :value="value" required />
 		<label class="label absolute text-opacity-75 bg-white px-3 py-1 pointer-events-none">{{ label }}</label>
 	</div>
 </template>
@@ -12,6 +12,7 @@ export default defineComponent({
 	props: {
 		label: String,
 		type: String,
+		value: String,
 	},
 });
 </script>
