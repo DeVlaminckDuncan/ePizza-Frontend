c2d7f37964a6810ca1e0d6cc102a19744d80ccde
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-11 03:13:02 +0100

    Takeaway: v-model implemented

--- src/presentations/pizza/shared/components/DropdownList.vue
+++ src/presentations/pizza/shared/components/DropdownList.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="relative">
-		<select class="select block border border-dark border-opacity-10 rounded shadow-md px-3 py-1 focus:outline-none w-full" required>
+		<select @change="changeSelectedValue" class="select block border border-dark border-opacity-10 rounded shadow-md px-3 py-1 focus:outline-none w-full" required>
 			<option v-if="label" disabled selected>{{ label }}</option>
 			<option v-for="option of options" :key="option.text" :value="option.text" :selected="option.selected">{{ option.text }}</option>
 		</select>
@@ -14,6 +14,19 @@ export default defineComponent({
 	props: {
 		label: String,
 		options: Array,
+		modelValue: {
+			type: String,
+		},
+	},
+
+	setup(props, { emit }) {
+		const changeSelectedValue = (event: any) => {
+			emit('update:modelValue', event.target.value);
+		};
+
+		return {
+			changeSelectedValue,
+		};
 	},
 });
 </script>
