e903c911e3b6dc3f63b475a68784e0d5e93bade5
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-14 19:27:38 +0100

    Store: set orderType done

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,30 @@ import { createStore } from 'vuex';
 
 import Pizza from '@/models/Pizza';
 
-export type State = {
+export type PizzasState = {
 	[pizzas: string]: Array<Pizza>;
+};
+
+export type OrderTypeState = {
+	[orderType: string]: string;
 };
 
 export enum MutationTypes {
 	ADD_PIZZA = 'addPizza',
+	SET_ORDER_TYPE = 'setOrderType',
 }
 
-export type Mutations<S = State> = {
+export type PizzaMutations<S = PizzasState> = {
 	[MutationTypes.ADD_PIZZA](state: S, payload: Pizza): void;
 };
 
-const state: State = {
+export type OrderTypeMutations<S = OrderTypeState> = {
+	[MutationTypes.SET_ORDER_TYPE](state: S, payload: string): void;
+};
+
+const state = {
 	pizzas: localStorage.pizzas ? JSON.parse(localStorage.pizzas) : [],
+	orderType: localStorage.orderType ? JSON.parse(localStorage.orderType) : '',
 };
 
 export default createStore({
@@ -32,6 +42,14 @@ export default createStore({
 			const pizzas = state.pizzas;
 			pizzas.push(pizza);
 			localStorage.setItem('pizzas', JSON.stringify(pizzas));
+		},
+
+		[MutationTypes.SET_ORDER_TYPE](state, { orderType, orderTypeData }) {
+			const data = {
+				orderType: orderType,
+				data: orderTypeData,
+			};
+			localStorage.setItem('orderType', JSON.stringify(data));
 		},
 	},
 });
