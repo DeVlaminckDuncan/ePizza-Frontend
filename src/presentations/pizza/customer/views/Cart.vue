c7ec884ac6f85c55368928282be8831b94f6ce44
Duncan <duncan.de.vlaminck@student.howest.be>
2020-11-29 18:13:51 +0100

    Cart: replaced store with idb

--- src/presentations/pizza/customer/views/Cart.vue
+++ src/presentations/pizza/customer/views/Cart.vue
@@ -6,22 +6,22 @@
 			<h1 class="font-semibold text-2xl">Your cart</h1>
 
 			<div v-if="state.pizzas && state.pizzas.length">
-				<div v-for="(pizza, index) of state.pizzas" :key="pizza.id" class="flex flex-wrap items-center border border-dark border-opacity-5 rounded-lg shadow-md p-3 mt-8 mb-4">
+				<div v-for="pizza of state.pizzas" :key="pizza.id" class="flex flex-wrap items-center border border-dark border-opacity-5 rounded-lg shadow-md p-3 mt-8 mb-4">
 					<div class="flex flex-col w-1/2 text-left pr-3">
-						<router-link :to="`/details/${pizza.pizzaUrl}`" class="text-xl">{{ pizza.name }}</router-link>
+						<router-link :to="`/details/${pizza.idbId}`" class="text-xl">{{ pizza.name }}</router-link>
 						<span class="opacity-80">{{ pizza.size }}</span>
 					</div>
 
 					<div class="flex justify-end items-center w-1/2 pl-3">
-						<ButtonExtraSmall @click="decreaseAmount(pizza, index)" text="-" color="red" />
+						<ButtonExtraSmall @click="decreaseAmount(pizza)" text="-" color="red" />
 						<span class="font-semibold px-2">{{ pizza.amount }}</span>
-						<ButtonExtraSmall @click="increaseAmount(pizza, index)" text="+" color="red" />
+						<ButtonExtraSmall @click="increaseAmount(pizza)" text="+" color="red" />
 					</div>
 
 					<div class="flex items-center w-full pt-3">
 						<div class="flex w-3/4">
-							<router-link :to="`/edit/${index}`" class="flex justify-center items-center bg-alpha-yellow text-white font-semibold text-lg w-24 h-8 rounded-lg shadow-md">Edit</router-link>
-							<ButtonSmall @click="removePizza(index)" class="ml-3" text="Remove" color="orange" />
+							<router-link :to="`/edit/${pizza.idbId}`" class="flex justify-center items-center bg-alpha-yellow text-white font-semibold text-lg w-24 h-8 rounded-lg shadow-md">Edit</router-link>
+							<ButtonSmall @click="removePizza(pizza)" class="ml-3" text="Remove" color="orange" />
 						</div>
 
 						<div class="w-1/4 font-semibold text-xl flex justify-end">{{ makePricePrettier((pizza.totalPrice ? pizza.totalPrice : pizza.price) * (pizza.amount ? pizza.amount : 1)) }}</div>
@@ -45,10 +45,11 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
-import store, { MutationTypes } from '@/store';
 
+import { getItems, editItem, deleteItem } from '@/utils/idb';
 import { makePricePrettier } from '@/utils/dataFormattings';
 import Pizza from '@/models/Pizza';
+import Topping from '@/models/Topping';
 import ButtonBig from '../components/ButtonBig.vue';
 import ButtonSmall from '../components/ButtonSmall.vue';
 import ButtonExtraSmall from '../components/ButtonExtraSmall.vue';
@@ -69,55 +70,95 @@ export default defineComponent({
 
 	setup() {
 		const state: State = reactive({
-			pizzas: store.getters.getPizzas(),
+			pizzas: [],
 			total: 0,
 		});
 
-		state.pizzas.forEach((pizza: Pizza) => {
-			if (!pizza.amount) {
-				pizza.amount = 1;
-			}
+		const getPizzas = async () => {
+			const data = await getItems('pizzas');
 
-			if (pizza.toppings && pizza.toppings.length) {
-				pizza.totalPrice = pizza.price + pizza.toppings.reduce((a, b) => a + (b.price ? b.price : 0) * (b.amount ? b.amount : 1), 0);
-			}
-		});
+			state.pizzas = data;
+
+			state.pizzas.forEach((pizza: Pizza) => {
+				if (!pizza.amount) {
+					pizza.amount = 1;
+				}
+
+				if (pizza.toppings && pizza.toppings.length) {
+					pizza.totalPrice = pizza.price + pizza.toppings.reduce((a, b) => a + (b.price ? b.price : 0) * (b.amount ? b.amount : 1), 0);
+				}
+			});
+
+			calculateTotal();
+		};
+
+		getPizzas();
 
 		const calculateTotal = () => {
 			state.total = state.pizzas.reduce((a, b) => a + (b.totalPrice ? b.totalPrice : b.price) * (b.amount ? b.amount : 1), 0);
 		};
 
-		calculateTotal();
+		const removeReactivityFromPizza = (pizza: Pizza) => {
+			const data = {
+				idbId: pizza.idbId,
+				id: pizza.id,
+				name: pizza.name,
+				price: pizza.price,
+				imgUrl: pizza.imgUrl,
+				amount: pizza.amount,
+				size: pizza.size,
+				toppings: [],
+			};
 
-		const increaseAmount = (pizza: Pizza, pizzaIndex: number) => {
+			if (pizza.toppings) {
+				pizza.toppings.forEach((t) => {
+					const topping: Topping = {
+						id: t.id,
+						name: t.name,
+						price: t.price,
+						amount: t.amount,
+					};
+					// @ts-ignore
+					data.toppings.push(topping);
+				});
+			}
+
+			return data;
+		};
+
+		const increaseAmount = async (pizza: Pizza) => {
 			pizza.amount!++;
 
 			calculateTotal();
 
-			store.commit(MutationTypes.EDIT_PIZZA, { pizzaIndex, pizza });
+			const data = removeReactivityFromPizza(pizza);
+
+			await editItem('pizzas', data);
 		};
 
-		const decreaseAmount = (pizza: Pizza, pizzaIndex: number) => {
+		const decreaseAmount = async (pizza: Pizza) => {
 			pizza.amount!--;
 
 			if (pizza.amount == 0) {
-				removePizza(pizzaIndex);
+				removePizza(pizza);
 			} else {
 				calculateTotal();
 
-				store.commit(MutationTypes.EDIT_PIZZA, { pizzaIndex, pizza });
+				const data = removeReactivityFromPizza(pizza);
+
+				await editItem('pizzas', data);
 			}
 		};
 
-		const removePizza = (pIndex: number) => {
-			const pizzaIndex = pIndex;
-			if (pizzaIndex >= 0) {
-				state.pizzas.splice(pizzaIndex, 1);
+		const removePizza = async (pizza: Pizza) => {
+			const index = state.pizzas.findIndex((p) => p.id == pizza.id);
+			state.pizzas.splice(index, 1);
 
-				calculateTotal();
+			calculateTotal();
 
-				store.commit(MutationTypes.REMOVE_PIZZA, pizzaIndex);
-			}
+			const data = removeReactivityFromPizza(pizza);
+
+			await deleteItem('pizzas', data);
 		};
 
 		const checkout = () => {};
