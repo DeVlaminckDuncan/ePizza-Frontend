8de388092cf1a2ca9c0f9780a52535ca4ce4d24c
Duncan <duncan.de.vlaminck@student.howest.be>
2020-12-01 00:06:36 +0100

    I18n: use plugin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { createApp } from 'vue';
+
+import i18n from './plugins/i18n';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
 import './assets/tailwind.css';
 
 createApp(App)
+	.use(i18n)
 	.use(store)
 	.use(router)
 	.mount('#app');
