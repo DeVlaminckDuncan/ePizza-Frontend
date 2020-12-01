67786f3e842927caa8316c21bf02c85007fc8320
Duncan <duncan.de.vlaminck@student.howest.be>
2020-12-01 14:43:03 +0100

    Order type: added translations

--- src/presentations/pizza/customer/views/OrderType.vue
+++ src/presentations/pizza/customer/views/OrderType.vue
@@ -1,14 +1,15 @@
 <template>
-	<NavigationBar text="Order type" />
+	<NavigationBar :text="$t('HEADER-TITLE-ORDERTYPE')" />
 
 	<main class="px-6 py-8 flex justify-center">
 		<div class="main">
-			<h1 class="font-semibold text-2xl">Choose the order type</h1>
+			<h1 class="font-semibold text-2xl">{{ $t('PAGE-INFO-ORDERTYPE') }}</h1>
+
 			<div class="flex justify-center mt-8">
-				<router-link to="/delivery" class="button-lg flex justify-center items-center text-white font-semibold text-xl bg-alpha-yellow h-16 rounded-lg shadow-md">Delivery</router-link>
+				<router-link to="/delivery" class="button-lg flex justify-center items-center text-white font-semibold text-xl bg-alpha-yellow h-16 rounded-lg shadow-md">{{ $t('ORDERTYPE-DELIVERY') }}</router-link>
 			</div>
 			<div class="flex justify-center mt-6">
-				<router-link to="/takeaway" class="button-lg flex justify-center items-center text-white font-semibold text-xl bg-alpha-orange h-16 rounded-lg shadow-md">Takeaway</router-link>
+				<router-link to="/takeaway" class="button-lg flex justify-center items-center text-white font-semibold text-xl bg-alpha-orange h-16 rounded-lg shadow-md">{{ $t('ORDERTYPE-TAKEAWAY') }}</router-link>
 			</div>
 		</div>
 	</main>
