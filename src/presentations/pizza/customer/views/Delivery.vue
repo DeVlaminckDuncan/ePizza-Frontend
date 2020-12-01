119392ed879cbefaf9865a7db177a5beead7dac0
Duncan <duncan.de.vlaminck@student.howest.be>
2020-12-01 14:47:28 +0100

    Delivery: added translations

--- src/presentations/pizza/customer/views/Delivery.vue
+++ src/presentations/pizza/customer/views/Delivery.vue
@@ -1,19 +1,19 @@
 <template>
-	<NavigationBar text="Delivery" :backIcon="true" previousPage="/" />
+	<NavigationBar :text="$t('ORDERTYPE-DELIVERY')" :backIcon="true" previousPage="/" />
 
 	<main class="px-6 py-8 flex justify-center">
 		<div class="main">
-			<h1 class="font-semibold text-2xl">Enter your address</h1>
+			<h1 class="font-semibold text-2xl">{{ $t('PAGE-INFO-DELIVERY') }}</h1>
 
 			<form class="w-full mt-8">
 				<div class="flex">
-					<InputField v-model="location.street" label="Street" class="w-3/4 mr-3" />
-					<InputField v-model="location.streetNo" label="No" class="w-1/4" />
+					<InputField v-model="location.street" :label="$t('INPUT-LABEL-STREET')" class="w-3/4 mr-3" />
+					<InputField v-model="location.streetNo" :label="$t('INPUT-LABEL-HOUSE-NUMBER')" class="w-1/4" />
 				</div>
 
-				<InputField v-model="location.city" label="City" />
+				<InputField v-model="location.city" :label="$t('INPUT-LABEL-CITY')" />
 
-				<ButtonWide @click="continueToMenu" text="Continue" color="red" />
+				<ButtonWide @click="continueToMenu" :text="$t('BUTTON-CONTINUE')" color="red" />
 			</form>
 		</div>
 	</main>
