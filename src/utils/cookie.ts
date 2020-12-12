f2a401f55e22920687e58e94c3b02b0b92bda80c
Duncan <duncan.de.vlaminck@student.howest.be>
2020-12-12 18:10:33 +0100

    Utils: added cookie.ts

---
+++ src/utils/cookie.ts
@@ -0,0 +1,21 @@
+export default {
+	get: (name: string): any => {
+		const cookieName = name + '=';
+		const decodedCookie = decodeURIComponent(document.cookie);
+		const ca = decodedCookie.split(';');
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1);
+			if (c.indexOf(cookieName) == 0) return c.substring(cookieName.length, c.length);
+		}
+		return '';
+	},
+
+	save: (name: string, value: string, expires?: string): any => {
+		let expiresString = '';
+		if (expires) {
+			expiresString = `; expires=${expires}`;
+		}
+		document.cookie = `${name}=${value}${expiresString}`;
+	},
+};
