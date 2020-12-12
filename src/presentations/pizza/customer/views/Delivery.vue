8718e4c199ddaad5741d5af1f98dd6ee7d30dee6
Duncan <duncan.de.vlaminck@student.howest.be>
2020-12-12 19:32:44 +0100

    Delivery: changed previousPage to "/ordertype"

--- src/presentations/pizza/customer/views/Delivery.vue
+++ src/presentations/pizza/customer/views/Delivery.vue
@@ -1,5 +1,5 @@
 <template>
-	<NavigationBar :text="$t('ORDERTYPE-DELIVERY')" :backIcon="true" previousPage="/" />
+	<NavigationBar :text="$t('ORDERTYPE-DELIVERY')" :backIcon="true" previousPage="/ordertype" />
 
 	<main class="px-3 sm:px-6 py-8 flex justify-center">
 		<div class="main">
